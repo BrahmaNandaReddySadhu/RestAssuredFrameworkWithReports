d861f8c094cb65bff7623d62f96eff34b45976df
Brahmananda Reddy Sadhu <sadhubrahmanandareddy@gmail.com>
2025-03-15 21:43:23 +0530

    Created the RestUtil and seperated the utils and test cases

---
+++ src/main/java/utils/RestUtils.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.util.Map;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class RestUtils {
+
+    public static  Response performPost(String endPoint, String requestPayload , Map<String,String> headers){
+        return RestAssured.given().log().all()
+                .baseUri(endPoint)
+                .contentType(ContentType.JSON)
+                .headers(headers)
+                .body(requestPayload)
+                .when()
+                .post()
+                .then().log().all()
+                .extract().response();
+    }
+
+}
--- src/test/java/Airlines/AirlineTests.java
+++ src/test/java/Airlines/AirlineTests.java
@@ -5,30 +5,32 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.RestUtils;
+
+import java.util.HashMap;
 
 public class AirlineTests {
 
     @Test
     public void createAirline() {
-        Response response = RestAssured.given().log().all()
-                .baseUri("https://restful-booker.herokuapp.com/booking")
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstname\" : \"Jim\",\n" +
-                        "    \"lastname\" : \"Brown\",\n" +
-                        "    \"totalprice\" : 111,\n" +
-                        "    \"depositpaid\" : true,\n" +
-                        "    \"bookingdates\" : {\n" +
-                        "        \"checkin\" : \"2018-01-01\",\n" +
-                        "        \"checkout\" : \"2019-01-01\"\n" +
-                        "    },\n" +
-                        "    \"additionalneeds\" : \"Breakfast\"\n" +
-                        "}")
-                .when()
-                .post()
-                .then().log().all()
-                .extract().response();
-        Assert.assertEquals(response.statusCode(), 200);
+
+        String baseUri= "https://restful-booker.herokuapp.com/booking";
+
+        String payload = "{\n" +
+                "    \"firstname\": \"Jim\",\n" +
+                "    \"lastname\": \"Brown\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2018-01-01\",\n" +
+                "        \"checkout\": \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+
+        Response response =RestUtils.performPost(baseUri,payload,new HashMap<>());
+        Assert.assertEquals(response.statusCode(),200);
+
 
     }
 
