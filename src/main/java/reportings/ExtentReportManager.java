a448211f093384c6f65873ad0429542d8f99e163
Brahmananda Reddy Sadhu <sadhubrahmanandareddy@gmail.com>
2025-03-16 11:39:31 +0530

    Adding extent reports

---
+++ src/main/java/reportings/ExtentReportManager.java
@@ -0,0 +1,24 @@
+package reportings;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentReportManager {
+
+    public static ExtentReports extentReports;
+
+
+    public static  void createInstance(String fileName,String reportName,String documentTitle){
+        ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(fileName);
+        extentSparkReporter.config().setReportName(reportName);
+        extentSparkReporter.config().setDocumentTitle(documentTitle);
+        extentSparkReporter.config().setTheme(Theme.DARK);
+        extentSparkReporter.config().setEncoding("UTF-8");
+
+
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentSparkReporter);
+
+    }
+}
